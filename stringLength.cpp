b47a0fc6a97e0621655481fae4662750f5d4c150
Vikram Markali <vikrammarkali007@gmail.com>
2022-12-03 14:56:51 +0530

    finding length of string

---
+++ stringLength.cpp
@@ -0,0 +1,18 @@
+/*
+    Created By : Vikram Markali
+*/
+
+#include <bits/stdc++.h>
+#define print(x) cout<<x<<;
+using namespace std;
+
+int main()
+{
+    char str[] = "Vikram Markali";
+    int i=0;
+    while(str[i]!= '\0'){
+        i++;
+    }
+    cout<<"Length of String: "<<i;
+    return 0;
+}
