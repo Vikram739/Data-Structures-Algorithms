a87c18e84a8dc7738e7d4ef559d90ff35a0cac8d
Vikram Markali <vikrammarkali007@gmail.com>
2022-12-03 14:55:57 +0530

    finding length of string

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -6,44 +6,13 @@
 #define print(x) cout<<x<<;
 using namespace std;
 
-struct Array
-{
-    int A[10];
-    int size;
-    int length;
-};
- void Display(struct Array arr)
- {
-    int i;
-    printf("\nElements are\n");
-    for(i=0;i<arr.length;i++)
-    {
-        printf("%d ",arr.A[i]);
-    }
- }
-void swap(int *x,int *y)
- {
-    int temp=*x;
-    *x=*y;
-    *y=temp;
- }
-int LinearSearch(struct Array *arr,int key)
-{
-    int i;
-    for(i=0;i<arr->length;i++)
-    {
-        if(key==arr->A[i])
-        {
-            swap(&arr->A[i],&arr->A[0]);
-            return i;
-        }
-    }
-    return -1;
-}
 int main()
 {
-    struct Array arr1={{2,23,14,5,6,9,8,12},10,8};
-    printf("%d",LinearSearch(&arr1,14));
-    Display(arr1);
+    char str[] = "Vikram Markali";
+    int i=0;
+    while(str[i]!= '\0'){
+        i++;
+    }
+    cout<<"Length of String: "<<i;
     return 0;
 }
