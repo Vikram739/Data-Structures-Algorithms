0f1639c6d283a2d6405e6e1775576bb02c7fa4b3
Vikram Markali <vikrammarkali007@gmail.com>
2022-12-22 15:09:58 +0530

    search node and place it at first position such that it will access with O(1) next time...

---
+++ Linked_List/searchNodeHead.cpp
@@ -0,0 +1,92 @@
+/*
+    Created By : Vikram Markali
+*/
+
+#include <bits/stdc++.h>
+#define print(x) cout<<x<<;
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}*first;
+
+
+void createNode(int A[],int n)
+{
+    struct Node *t,*last;
+    // first = (struct Node *)malloc(sizeof(struct Node));
+    first = new Node;
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+    
+    for(int i=1;i<n;i++)
+    {
+        // t = (struct Node *)malloc(sizeof(struct Node));
+        t = new Node;
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void displayList(struct Node *p)
+{
+    while(p != NULL)
+    {
+        cout<<p->data<<" ";
+        p = p->next;
+    }
+}
+
+Node* searchNode(struct Node *p, int key)
+{
+    while(p != NULL)
+    {
+        if(key == p->data)
+        {
+            // cout<<"\nKey is found!!";
+            return p;
+        }
+        p= p->next;
+    }
+    // cout<<"\nNot found!!";
+    return NULL;
+}
+
+Node* searchNodeHead(Node *p, int key)
+{
+    Node *q =  NULL;
+    if(key == p->data)
+    {
+        return p;
+    }
+    
+    while( p != NULL)
+    {
+        if(key == p->data)
+        {
+            q->next = p->next;
+            p->next = first;
+            first = p;
+            return p;
+        }
+        q = p;
+        p = p->next;
+    }
+    return NULL;
+}
+int main()
+{
+    int A[] = {10,20,30,60,40,50};
+    createNode(A,6);
+    displayList(first);
+
+    cout<<"\nNode :"<<searchNodeHead(first,60);
+    cout<<"\nNode :"<<searchNodeHead(first,60);
+    
+    return 0;
+}
