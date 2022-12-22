b880b25c34b6c44917156e63621d2aa64f723924
Vikram Markali <vikrammarkali007@gmail.com>
2022-12-22 18:54:26 +0530

    insert at  begining started...

---
+++ Linked_List/insertAtBegining.cpp
@@ -0,0 +1,56 @@
+/*
+    Created By : Vikram Markali
+*/
+
+#include <bits/stdc++.h>
+#define print(x) cout<<x<<;
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}*first;
+
+
+void createNode(int A[],int n)
+{
+    struct Node *t,*last;
+    // first = (struct Node *)malloc(sizeof(struct Node));
+    first = new Node;
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+    
+    for(int i=1;i<n;i++)
+    {
+        // t = (struct Node *)malloc(sizeof(struct Node));
+        t = new Node;
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void displayList(struct Node *p)
+{
+    cout<<"Linked List: ";
+    while(p != NULL)
+    {
+        cout<<p->data<<" ";
+        p = p->next;
+    }
+}
+
+int main()
+{
+    int A[] = {10,20,30,60,40,50};
+    createNode(A,6);
+    displayList(first);
+
+    
+    
+    
+    return 0;
+}
