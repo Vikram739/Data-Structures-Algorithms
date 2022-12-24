61e134358ef2b912dc6f07ecab00d162bc5d1c9c
Vikram Markali <vikrammarkali007@gmail.com>
2022-12-24 22:59:16 +0530

    delete Node from linked list...

--- Linked_List/deleteNode.cpp
+++ Linked_List/deleteNode.cpp
@@ -6,8 +6,50 @@
 #define print(x) cout<<x<<;
 using namespace std;
 
+struct Node
+{
+    int data;
+    struct Node *next;
+}*first;
+
+void createList(int A[],int n)
+{
+    struct Node *t,*last;
+    first = (struct Node*)malloc(sizeof(Node));
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for(int i=1;i<n;i++)
+    {
+        t =(struct Node*) malloc(sizeof(Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+
+    }
+}
+
+void displayList(struct Node *p)
+{
+    cout<<"\nLinked List: ";
+    while(p != NULL)
+    {
+        cout<<p->data<<" ";
+        p = p->next;
+    }
+}
+
+int deleteNode(int pos)
+{
+    struct Node
+}
 int main()
 {
-    
+    int A[] = {10,20,30,40,50};
+
+    createList(A,5);
+    displayList(first);
     return 0;
 }
