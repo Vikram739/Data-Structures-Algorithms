72342c57ee4f3ac4239239385707ebdfc82f83f2
Vikram Markali <vikrammarkali007@gmail.com>
2022-12-25 14:13:14 +0530

    check linked list is sorted or not...

--- Linked List/checkSortedOrNot.cpp
+++ Linked List/checkSortedOrNot.cpp
@@ -45,7 +45,17 @@ void displayList(struct Node *p)
 
 bool checkSorted(struct Node *p)
 {
-    
+    int x = INT_MIN;
+    while(p != NULL)
+    {
+        if(p->data < x)
+        {
+            return false;     // false = 0;
+        }
+        x = p->data;
+        p = p->next;
+    }
+    return true;        //true = 1;
 }
 
 int main()
@@ -54,6 +64,8 @@ int main()
 
     createList(A,5);
     displayList(first);
+
+    cout<<"\nCheck Linked List Sorted Or Not: "<<checkSorted(first);
     
 
     return 0;
