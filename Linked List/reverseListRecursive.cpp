56f33ec53bad308aa404716cc3f01c99ea893a9e
Vikram Markali <vikrammarkali007@gmail.com>
2022-12-28 11:58:10 +0530

    reverse linked list using recursion...

---
+++ Linked List/reverseListRecursive.cpp
@@ -0,0 +1,72 @@
+/*
+    Created By : Vikram Markali
+*/
+
+#include <bits/stdc++.h>
+#define print(x) cout<<x<<;
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}*first;
+
+void createList(int A[],int n)
+{
+    struct Node *t,*last;
+    // first = (struct Node*)malloc(sizeof(Node));
+    first = new Node;
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for(int i=1;i<n;i++)
+    {
+        // t =(struct Node*) malloc(sizeof(Node));
+        t = new Node;
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+
+    }
+}
+
+void displayList(struct Node *p)
+{
+    cout<<"\nLinked List: ";
+    while(p != NULL)
+    {
+        cout<<p->data<<" ";
+        p = p->next;
+    }
+}
+
+// reverse a linked list using recursion...
+void reverseListR(struct Node *q, struct Node *p)
+{
+    if(p != NULL)
+    {
+        reverseListR(p,p->next);
+        p->next = q;
+    }
+    else
+    {
+        first = q;
+    }
+}
+
+
+int main()
+{
+    int A[] = {10,20,30,40,50};
+
+    createList(A,5);
+    displayList(first);
+
+    reverseListR(NULL,first);
+    displayList(first);
+
+    return 0;
+}
