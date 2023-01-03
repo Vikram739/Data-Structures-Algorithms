a9dbf40a9d96defd197ce3db3f7b556661268fdd
Vikram Markali <vikrammarkali007@gmail.com>
2023-01-04 01:00:50 +0530

    Display circular linked list recursive completed...

--- Linked List/displayCircularLinkedList.cpp
+++ Linked List/displayCircularLinkedList.cpp
@@ -47,17 +47,18 @@ void displayCircular(struct Node *head)
     }while(p != head);
 }
 
-void displayCircularRecursive(struct Node *head)
+void displayCircularRecursive(struct Node *p)
 {
-    struct Node *p = head;
+    // struct Node *p = head;
     static int flg = 0;
     
-    while(p != head || flg == 0)
+    if(p != first || flg == 0)
     {
         flg = 1;
         cout<<p->data<<" ";
         displayCircularRecursive(p->next);
     }
+    flg=0;
 }
 int main()
 {
