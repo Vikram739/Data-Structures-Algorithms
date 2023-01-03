64afbea6ed02ab1487f400a0ca4abed28e8cee37
Vikram Markali <vikrammarkali007@gmail.com>
2023-01-03 10:21:04 +0530

    Display circular linked list...

--- Linked List/displayCircularLinkedList.cpp
+++ Linked List/displayCircularLinkedList.cpp
@@ -6,8 +6,39 @@
 #define print(x) cout<<x<<;
 using namespace std;
 
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}*first;
+
+void createList(int A[],int n)
+{
+    struct Node *t,*last;
+    // first = (struct Node*)malloc(sizeof(Node));
+    first = new Node;
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for(int i=1;i<n;i++)
+    {
+        // t =(struct Node*) malloc(sizeof(Node));
+        t = new Node;
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+
+    }
+    last->next = first;
+}
+
 int main()
 {
-    
+    int A[] = {10,20,30,40,50};
+
+    createList(A,5);
     return 0;
 }
