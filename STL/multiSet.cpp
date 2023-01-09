d5add032e7617a209411c8d6a95de25f1a8bf48e
Vikram Markali <vikrammarkali007@gmail.com>
2023-01-09 12:28:14 +0530

    multiset simple example

--- STL/multiSet.cpp
+++ STL/multiSet.cpp
@@ -8,16 +8,16 @@ using namespace std;
 
 int main()
 {
-    // multiset <int> s;
-    // s.insert(10);
-    // s.insert(20);
-    // s.insert(10);
-    // s.insert(20);
-    // s.insert(50);
+    multiset <int> s;
+    s.insert(10);
+    s.insert(20);
+    s.insert(10);
+    s.insert(20);
+    s.insert(50);
 
-    // for(auto i: s)
-    // {
-    //     cout<<i<<" ";
-    // }
+    for(auto i: s)
+    {
+        cout<<i<<" ";
+    }
     return 0;
 }
