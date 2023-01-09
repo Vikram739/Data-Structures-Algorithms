1319f1e4dbd40569cf625c7e06386777547651a2
Vikram Markali <vikrammarkali007@gmail.com>
2023-01-09 12:27:34 +0530

    multiset simple example

---
+++ STL/multiSet.cpp
@@ -0,0 +1,23 @@
+/*
+    Created By : Vikram Markali
+*/
+
+#include <bits/stdc++.h>
+#define print(x) cout<<x<<;
+using namespace std;
+
+int main()
+{
+    // multiset <int> s;
+    // s.insert(10);
+    // s.insert(20);
+    // s.insert(10);
+    // s.insert(20);
+    // s.insert(50);
+
+    // for(auto i: s)
+    // {
+    //     cout<<i<<" ";
+    // }
+    return 0;
+}
