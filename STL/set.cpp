ff39e2844507aa825b8112eccdd32ee5e39fd4f2
Vikram Markali <vikrammarkali007@gmail.com>
2023-01-09 12:34:28 +0530

    set example of STL

--- STL/set.cpp
+++ STL/set.cpp
@@ -8,6 +8,17 @@ using namespace std;
 
 int main()
 {
-    
+    // It stores unique values( no duplicates)...and returns in acending order(internally implements min heap)...
+    set <int> s;
+    s.insert(10);
+    s.insert(20);
+    s.insert(10);
+    s.insert(20);
+    s.insert(50);
+
+    for(auto i: s)
+    {
+        cout<<i<<" ";
+    }
     return 0;
 }
