42230683456efe433cd3338dcd44b94ac0e2788b
Vikram Markali <vikrammarkali007@gmail.com>
2023-01-11 09:44:16 +0530

    unordered map example form gfg

--- STL/unorderedMapDemo.cpp
+++ STL/unorderedMapDemo.cpp
@@ -1,13 +1,26 @@
-/*
-    Created By : Vikram Markali
-*/
 
-#include <bits/stdc++.h>
-#define print(x) cout<<x<<;
+// C++ program to demonstrate 
+// functionality of unordered_map
+#include <iostream>
+#include <unordered_map>
 using namespace std;
-
+  
+// Driver code
 int main()
 {
-    
-    return 0;
-}
+  // Declaring umap to be of 
+  // <string, int> type key 
+  // will be of STRING type 
+  // and mapped VALUE will
+  // be of int type
+  unordered_map<string, int> umap;
+  
+  // inserting values by using [] operator
+  umap["GeeksforGeeks"] = 10;
+  umap["Practice"] = 20;
+  umap["Contribute"] = 30;
+  
+  // Traversing an unordered map
+  for (auto x : umap)
+    cout << x.first << " " << 
+            x.second << endl;
