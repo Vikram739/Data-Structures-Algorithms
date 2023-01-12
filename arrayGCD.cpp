91f9903a459248549664276caeaee4ab84585d66
Vikram Markali <vikrammarkali007@gmail.com>
2023-01-12 09:11:40 +0530

    calculate array GCD

--- arrayGCD.cpp
+++ arrayGCD.cpp
@@ -6,8 +6,17 @@
 #define print(x) cout<<x<<;
 using namespace std;
 
+void display(int mortal[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<mortal[i]<<" ";
+    }
+    cout<<endl;
+}
 int main()
 {
-    
+    int mortal = {10,20,30,40,50};
+    cout<<"Array: "<<display(mortal,5);
     return 0;
 }
