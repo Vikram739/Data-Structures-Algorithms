860ccaee20db02b3f285771ae4cc62bca3f657f1
Vikram Markali <vikrammarkali007@gmail.com>
2023-01-13 07:54:53 +0530

    Count the number of bits in in an integer...

---
+++ countBits.cpp
@@ -0,0 +1,26 @@
+/*
+    Created By : Vikram Markali
+*/
+
+#include <bits/stdc++.h>
+#define print(x) cout<<x<<;
+using namespace std;
+
+
+// C++ program to find total bit in given number
+
+unsigned countBits(unsigned int number)
+{    
+    
+    // log function in base 2
+    // take only integer part
+    return (int)log2(number)+1;
+}
+  
+// Driven program    
+int main()
+{
+    unsigned int num = 65;
+    std::cout<<countBits(num)<<'\n';
+    return 0;
+}
