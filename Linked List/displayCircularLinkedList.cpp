92e8c4d02135de136a17d46cdd908cc671283b3d
Vikram Markali <vikrammarkali007@gmail.com>
2023-01-31 16:15:15 +0530

    display Circular linked list...

--- Linked List/displayCircularLinkedList.cpp
+++ Linked List/displayCircularLinkedList.cpp
@@ -62,9 +62,9 @@ void displayCircularRecursive(struct Node *p)
 }
 int main()
 {
-    int A[] = {10,20,30,40,50,60};
+    int A[] = {10,20,30,40,50};
 
-    createList(A,6);
+    createList(A,5);
     displayCircular(first);
     cout<<"\nRecursive Circular Linked List: ";
     displayCircularRecursive(first);
