e22741686a0d5d149006dc6725304a62df908cd8
Vikram Markali <vikrammarkali007@gmail.com>
2023-01-31 21:03:40 +0530

    create Doubly Linked List...

---
+++ Linked List/createDoublyLinkedList.cpp
@@ -0,0 +1,63 @@
+/*
+    Created By : Vikram Markali
+*/
+
+#include <bits/stdc++.h>
+#define print(x) cout<<x<<;
+using namespace std;
+
+struct Node
+{
+    struct Node *prev;
+    int data;
+    struct Node *next;
+}*first = NULL;
+
+void createList(int A[], int n)
+{
+    struct Node *t,*last;
+
+    first = (struct Node*)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->prev = NULL;
+    first->next = NULL;
+    last = first;
+
+    for(int i=1;i<n;i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->prev = last;
+        t->next = last->next;
+        last->next = t;
+        last = t;
+    }
+}
+int Length(struct Node *p)
+{
+    int len = 0;
+    while(p != NULL)
+    {
+        len++;
+        p = p->next;
+    }
+    return len;
+}
+
+void displayList(struct Node *p)
+{
+    while(p != NULL)
+    {
+        cout<<p->data<<" ";
+        p = p->next;
+    }
+}
+int main()
+{
+    int A[] = {10,20,30,40,50};
+
+    createList(A,5);
+    displayList(first);
+    cout<<"\nLenth of List: "<<Length(first);
+    return 0;
+}
