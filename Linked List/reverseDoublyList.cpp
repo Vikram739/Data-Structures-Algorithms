d794edea60a3f75af041a5a7dc8d64b87f2ce6cb
Vikram Markali <vikrammarkali007@gmail.com>
2023-02-01 00:21:12 +0530

    Doubly Linked List...

---
+++ Linked List/reverseDoublyList.cpp
@@ -0,0 +1,157 @@
+/*
+    Created By : Vikram Markali
+*/
+
+#include <bits/stdc++.h>
+#define print(x) cout<<x<<;
+using namespace std;
+
+struct Node
+{
+    struct Node *prev;
+    int data;
+    struct Node *next;
+}*first = NULL;
+
+void createList(int A[], int n)
+{
+    struct Node *t,*last;
+
+    // first = (struct Node*)malloc(sizeof(struct Node));
+    first = new Node;
+    first->data = A[0];
+    first->prev = NULL;
+    first->next = NULL;
+    last = first;
+
+    for(int i=1;i<n;i++)
+    {
+        // t = (struct Node *)malloc(sizeof(struct Node));
+        t = new Node;
+        t->data = A[i];
+        t->prev = last;
+        t->next = last->next;
+        last->next = t;
+        last = t;
+    }
+}
+int Length(struct Node *p)
+{
+    int len = 0;
+    while(p != NULL)
+    {
+        len++;
+        p = p->next;
+    }
+    return len;
+}
+
+void insertDoublyList(struct Node *p, int index, int x)
+{
+    struct Node *t;
+    if(index < 0 || index > Length(p))
+    {
+        return;
+    }
+    if(index == 0)
+    {
+        // t = (struct Node*)malloc(sizeof(struct Node));
+        t = new Node;
+        t->data = x;
+        t->prev = NULL;
+        t->next = first;
+        first->prev = t;
+        first = t;
+    }
+    else
+    {
+        for(int i=0;i<index-1;i++)
+        {
+            p = p->next;      
+        }
+        // t = (struct Node*)malloc(sizeof(struct Node));
+        t = new Node;
+        t->data = x;
+        t->prev = p;
+        t->next = p->next;
+        if(p->next)
+        {
+            p->next->prev = t;
+        }
+        p->next = t;
+    }
+}
+
+void deleteDoublyList(struct Node *p, int pos)
+{
+    if(pos < 1 || pos > Length(p))
+    {
+        return;
+    }
+    if(pos == 1)
+    {
+        first = first->next;
+        if(first)
+        {
+            first->prev = NULL;
+        }
+    }
+    else
+    {
+        for(int i=0;i<pos-1;i++)
+        {
+            p = p->next;
+        }
+        p->prev->next = p->next;
+        if(p->next)
+        {
+            p->next->prev = p->prev;
+        }
+    }
+}
+void reverseList(struct Node *p)
+{
+    struct Node *temp;
+    while(p != NULL)
+    {
+        temp = p->next;
+        p->next = p->prev;
+        p->prev = temp;
+
+        p = p->prev;
+        if(p != NULL && p->next == NULL)
+        {
+            first = p;
+        }
+    }
+}
+void displayList(struct Node *p)
+{
+    cout<<"Doubly List: ";
+    while(p != NULL)
+    {
+        cout<<p->data<<" ";
+        p = p->next;
+    }
+    cout<<"\n";
+}
+int main()
+{
+    int A[] = {10,20,30,40,50};
+
+    createList(A,5);
+    displayList(first);
+    // cout<<"\nLenth of List: "<<Length(first);
+
+    insertDoublyList(first,2,25);   // 2 means after 2...
+    displayList(first);
+
+    deleteDoublyList(first,4);
+    displayList(first);
+
+    reverseList(first);
+    cout<<"\nReversed List: ";
+    displayList(first);
+
+    return 0;
+}
