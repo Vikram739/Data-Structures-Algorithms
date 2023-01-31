b62a63b67ff880b8b675991ff65adb3dffa9ab4b
Vikram Markali <vikrammarkali007@gmail.com>
2023-01-31 19:43:23 +0530

    delete Node from Circular Linked list...

--- Linked List/deleteNodeCircularList.cpp
+++ Linked List/deleteNodeCircularList.cpp
@@ -45,47 +45,6 @@ void createList(int A[],int n)
     last->next = first;
 }
 
-void insertCircularList(Node *p , int index, int x)
-{
-    Node *t;
-    if(index<0 || index>Length(p))
-    {
-        cout<<"\nInvalid index...";
-        return;
-    }
-    if(index == 0)
-    {
-        // t = (struct Node*)malloc(sizeof(struct Node));
-        t = new Node;
-        t->data = x;
-        if(first == NULL)
-        {
-            first = t;
-            first->next = first;
-        }
-        else
-        {
-            while(p->next != first)
-            {
-                p = p->next;
-            }
-            p->next = t;
-            t->next = first;
-            first = t;
-        }
-    }
-    else
-    {
-        for(int i=0;i<index-1;i++)
-        {
-            p = p->next;
-        }
-        t = new Node;
-        t->data = x;
-        t->next = p->next;
-        p->next = t;
-    }
-}
 void deleteCircularList(Node *p,int pos)
 {
     if(pos <0 || pos>Length(p))
@@ -128,9 +87,6 @@ int main()
     int A[] = {10,20,30,40,50};
 
     createList(A,5);
-    displayCircular(first);
-
-    insertCircularList(first,3,35);  // 3 means after third position...
     displayCircular(first);
 
     deleteCircularList(first,4);
