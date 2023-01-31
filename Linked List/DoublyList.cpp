dcba765270f7618369c88405440196b539355f83
Vikram Markali <vikrammarkali007@gmail.com>
2023-01-31 20:54:38 +0530

    Doubly Linked List...

--- Linked List/DoublyList.cpp
+++ Linked List/DoublyList.cpp
@@ -6,8 +6,47 @@
 #define print(x) cout<<x<<;
 using namespace std;
 
+struct Node
+{
+    struct Node *prev;
+    int data;
+    struct Node *next;
+}*first = NULL;
+
+void createList(int A[], int n)
+{
+    struct Node *t,*last;
+
+    first = (struct Node*)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->prev = NULL;
+    first->next = NULL;
+    last = first;
+
+    for(int i=1;i<n;i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->prev = last;
+        t->next = last->next;
+        last->next = t;
+        last = t;
+    }
+}
+
+void displayList(struct Node *p)
+{
+    while(p != NULL)
+    {
+        cout<<p->data<<" ";
+        p = p->next;
+    }
+}
 int main()
 {
-    
+    int A[] = {10,20,30,40,50};
+
+    createList(A,5);
+    displayList(first);
     return 0;
 }
