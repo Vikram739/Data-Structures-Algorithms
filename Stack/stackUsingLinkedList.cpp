39d05a10457e8e4b46affe3a3cf129bb08c2db5f
Vikram Markali <vikrammarkali007@gmail.com>
2023-02-13 11:30:13 +0530

    Stack using Linked List...

--- Stack/stackUsingLinkedList.cpp
+++ Stack/stackUsingLinkedList.cpp
@@ -48,12 +48,35 @@ int pop()
     return x;
 }
 
-void Display()
+void Display(struct Node *p)
 {
-    
+    // struct Node *p;
+    if(top == NULL)
+    {
+        cout<<"\nStack is Empty!!";
+    }
+    else
+    {
+        cout<<"\nLinked List: ";
+        while(p != NULL)
+        {
+            cout<<p->data<<" ";
+            p = p->next;
+        }
+        cout<<endl;
+    }
 }
 int main()
 {
-    
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+    push(50);
+
+    Display(top);
+    cout<<"\nPoped Element: "<<pop();
+    cout<<"\nPoped Element: "<<pop();
+    Display(top);
     return 0;
 }
