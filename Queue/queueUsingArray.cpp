4e336d2255bb003a1777c79ae4482eaaabbaee24
Vikram Markali <vikrammarkali007@gmail.com>
2023-02-21 00:03:50 +0530

    Queue using Array...

--- Queue/queueUsingArray.cpp
+++ Queue/queueUsingArray.cpp
@@ -14,9 +14,36 @@ struct Queue
     int *Q;
 };
 
+void createQueue(struct Queue *q, int size)
+{
+    q->size = size;
+    q->front = q->rear = -1;
+    q->Q = (int *)malloc(q->size*sizeof(int));
+}
+void enqueue(Queue *q, int x)
+{
+    if(q->rear == q->size-1)
+    {
+        cout<<"\nQueue is Full!!";
+    }
+    else
+    {
+        q->rear++;
+        q->Q[q->rear] = x;
+        
+    }
+}
 
+void 
 int main()
 {
-    
+    struct Queue *q;
+    createQueue(q, 5);
+
+    enqueue(q,10);
+    enqueue(q,20);
+    enqueue(q,30);
+
+    dqueue(q);
     return 0;
 }
